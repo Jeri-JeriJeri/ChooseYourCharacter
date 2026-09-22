56fa194e583859bf5ddfbaa7c4216178dbda0b9b
Jeri-JeriJeri <ivanovski.bojan10@gmail.com>
2026-09-22 20:33:54 +0200

    On hover pic of the char replaces lockedChar

---
+++ app.js
@@ -0,0 +1,28 @@
+const lockedChar = document.getElementById("lockedChar")
+
+const gohan = document.getElementById("gohan");
+const spiderman = document.getElementById("spiderman");
+const dorian = document.getElementById("dorian");
+const revanent = document.getElementById("revanent");
+const dean = document.getElementById("dean");
+const grizzly = document.getElementById("grizzly");
+
+
+gohan.addEventListener("mouseenter", () => {
+  lockedChar.src = "lockedChar/gohan.jpg"
+})
+spiderman.addEventListener("mouseenter", () => {
+  lockedChar.src = "lockedChar/spiderman.jpg"
+})
+dorian.addEventListener("mouseenter", () => {
+  lockedChar.src = "lockedChar/dorian.jpg"
+})
+revanent.addEventListener("mouseenter", () => {
+  lockedChar.src = "lockedChar/revanent.jpg"
+})
+dean.addEventListener("mouseenter", () => {
+  lockedChar.src = "lockedChar/dean.jpg"
+})
+grizzly.addEventListener("mouseenter", () => {
+  lockedChar.src = "lockedChar/grizzly.jpg"
+})
