5ef6546b80b513bfbec51bd56af775061469286f
Jeri-JeriJeri <ivanovski.bojan10@gmail.com>
2026-09-24 23:32:32 +0200

    Delay fixed

--- charWebsite/script.js
+++ charWebsite/script.js
@@ -32,7 +32,7 @@ const quote = document.getElementById("audio");
 // # FIX VOLUME ON DORIAN AND GRIZZLY ##
 // #######################################
 function delayedAudio () {
-setTimeout(playAudio, 550);
+setTimeout(playAudio, 750);
 
 function playAudio () {
   
